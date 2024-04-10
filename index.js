e207b17529d0b7e577a73fda85bae92ed3ebe7a0
Rangassamy <enzorangassamy@gmail.com>
2024-04-09 20:01:05 -0400

    added the tp without correction

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 // Créer la logique de compte à rebours
 
-choice.value = 5 minutes
-
-choice.value * 60 = totalSeconds
-
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
-
 // Créer un événement à la validation du form pour lancer le compte à rebours
+
+//-------------------------------------------//
+
+// choice.value = 5 minutes;
+
+// choice.value * 60 = totalSeconds;
+
+// let minutes = Math.floor(totalSeconds / 60);
+
+// let seconds = totalSeconds % 60;
+
+//-------------------------------------------//
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let totalSeconds = choice.value * 60;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+  setInterval(() => {
+    if (seconds > 0) {
+      seconds--;
+      countdownDisplay.innerHTML = `<h2>${minutes}:${seconds}</h2>`;
+    } else if (minutes > 0) {
+      minutes--;
+      seconds = 59;
+      countdownDisplay.innerHTML = `<h2>${minutes}:${seconds}</h2>`;
+    } else {
+      countdownDisplay.innerHTML = `<h2>C'est terminé !</h2>`;
+    }
+  }, 1000);
+});
